fe816eabe6bfb1f73f2711ebfbe746a78c34d267
Shae-Marie Small <shaemarie.small@gmail.com>
2021-10-30 23:39:53 -0400

    Added initial starter files

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ window.onload = function(){
 
 function getSuperheroes(){
 
+    event.preventDefault()
+    var querty_text= document.querySelector(".querty_text").value
+    console.log(querty_text)
+
     fetch('http://localhost:80/info2180-lab4/superheroes.php')
     .then(response =>response.text())
     .then(data => alert(data))
     .catch(error => console.log(error)) 
-    
 
 }
 
